fc99d3bf7d059cae8c6b5240c5ff5162458737c0
Tomáš Král <tomaskral95@gmail.com>
2018-04-02 18:56:12 +0200

    Repairing tests

--- src/main/java/com/buddyservice/repository/IStudentRepository.java
+++ src/main/java/com/buddyservice/repository/IStudentRepository.java
@@ -4,7 +4,11 @@ import com.buddyservice.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IStudentRepository extends JpaRepository<Student, Long> {
 
+    @Override
+    Optional<Student> findById(Long aLong);
 }
