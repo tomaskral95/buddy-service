acc0ba27c7daf5eb5ceca519bfec27870a213992
Tomáš Král <tomaskral95@gmail.com>
2018-04-03 00:37:00 +0200

    Transfering tests from MySQL to H2 In-Memory DB

--- src/main/java/com/buddyservice/domain/Student.java
+++ src/main/java/com/buddyservice/domain/Student.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "STUDENT")
+@Table(name = "STUDENT ")
 public class Student {
 
     private Long id;
--- src/test/groovy/com/buddyservice/PersistEntityIT.groovy
+++ src/test/groovy/com/buddyservice/PersistEntityIT.groovy
@@ -4,9 +4,12 @@ import com.buddyservice.domain.Adresa
 import com.buddyservice.domain.Pohlavi
 import com.buddyservice.domain.Student
 import com.buddyservice.repository.IStudentRepository
+import org.h2.tools.Server
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+
+import java.sql.SQLException
 
 @ContextConfiguration(locations = ["classpath:jdbc-connection-test-context.xml"])
 class PersistEntityIT extends Specification {
@@ -40,6 +43,7 @@ class PersistEntityIT extends Specification {
                     email: "krat08@vse.cz"
             )
         when: "Persist and select from database"
+            startH2TcpServer()
             buddyRepository.save(student)
             def foundStudents = buddyRepository.findAll()
             def obtainedStudent = foundStudents.get(0)
@@ -66,6 +70,16 @@ class PersistEntityIT extends Specification {
             assert student.adresy[1].mesto == obtainedStudent.adresy[1].mesto
             assert student.adresy[1].ulice == obtainedStudent.adresy[1].ulice
             assert student.adresy[1].cisloPopisne == obtainedStudent.adresy[1].cisloPopisne
+    }
+
+    def void startH2TcpServer() {
+        try {
+            Server server = Server.createTcpServer().start()
+            println "Server started and connection is open."
+            println "URL: jdbc:h2:" + server.getURL() + "/mem:test"
+        } catch (SQLException e) {
+            throw new IllegalStateException("Could not start H2 server", e)
+        }
     }
 
 }
